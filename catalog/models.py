7cefbc07abc11b83d46f1bd1620e6bd9c7dd8908
Ostap <ostapbojko04@gmail.com>
2024-06-22 17:16:53 +0300

    add models Task&Tag in models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    done = models.BooleanField(default=False)
+    tag = models.JSONField()
+
+    def __str__(self):
+        return self.content
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
