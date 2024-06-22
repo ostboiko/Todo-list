bc7938fb4076d5b4c2ff4acced1605fe3ea611ad
Ostap <ostapbojko04@gmail.com>
2024-06-22 17:20:11 +0300

    update model Task

--- catalog/models.py
+++ catalog/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 
-class Task(models.Model):
-    content = models.TextField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateTimeField(null=True, blank=True)
-    done = models.BooleanField(default=False)
-    tag = models.JSONField()
-
-    def __str__(self):
-        return self.content
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
@@ -19,4 +8,13 @@ class Tag(models.Model):
         return self.name
 
 
+class Task(models.Model):
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    done = models.BooleanField(default=False)
+    tag = models.JSONField(Tag, related_name='tasks')
+
+    def __str__(self):
+        return self.content
 
