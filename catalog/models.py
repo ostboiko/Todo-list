5f5e1ee66613b6349b1f8be661affdaac1d7ddde
Ostap <ostapbojko04@gmail.com>
2024-06-24 17:28:23 +0300

    add models in model.py

--- catalog/models.py
+++ catalog/models.py
@@ -9,11 +9,11 @@ class Tag(models.Model):
 
 
 class Task(models.Model):
-    content = models.TextField(max_length=255)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     done = models.BooleanField(default=False)
-    tags = models.JSONField(Tag, related_name="tasks")
+    tags = models.ManyToManyField(Tag, related_name="tasks")
 
     def __str__(self):
         return self.content
