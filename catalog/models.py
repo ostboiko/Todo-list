bdbe33a9d96c48cc49551eb3ea7b274fc67747b4
Ostap <ostapbojko04@gmail.com>
2024-06-24 21:45:35 +0300

    edit mox_lenght in models.py

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
@@ -17,4 +17,3 @@ class Task(models.Model):
 
     def __str__(self):
         return self.content
-
