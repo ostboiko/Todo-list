1ba6821b4301241f9653c5452edc3bfff3336855
Ostap <ostapbojko04@gmail.com>
2024-07-04 22:43:25 +0300

    update import in urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,18 @@
-# catalog/urls.py
-
+# urls.py
 from django.urls import path
-from . import views
+from .views import (
+    HomeView, TaskCreateView, TaskUpdateView, TagListView,
+    TagCreateView, TagUpdateView, TagDeleteView, TaskDeleteView, TaskToggleCompleteView
+)
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('task/add/', views.add_task, name='add_task'),
-    path('task/<int:pk>/update/', views.update_task, name='update_task'),
-    path('task/<int:pk>/delete/', views.delete_task, name='delete_task'),
-    path('task/<int:pk>/complete/', views.complete_task, name='complete_task'),
-    path('tags/', views.tag_list, name='tag_list'),
-    path('tag/add/', views.add_tag, name='add_tag'),
-    path('tag/<int:pk>/update/', views.update_tag, name='update_tag'),
-    path('tag/<int:pk>/delete/', views.delete_tag, name='delete_tag'),
+    path('', HomeView.as_view(), name='home'),
+    path('task/new/', TaskCreateView.as_view(), name='add_task'),
+    path('task/<int:pk>/edit/', TaskUpdateView.as_view(), name='update_task'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='delete_task'),
+    path('task/<int:pk>/complete/', TaskToggleCompleteView.as_view(), name='complete_task'),
+    path('tags/', TagListView.as_view(), name='tag_list'),
+    path('tag/new/', TagCreateView.as_view(), name='add_tag'),
+    path('tag/<int:pk>/edit/', TagUpdateView.as_view(), name='update_tag'),
+    path('tag/<int:pk>/delete/', TagDeleteView.as_view(), name='delete_tag'),
 ]
