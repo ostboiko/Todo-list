0b7bd2a1d04056dc5cdf69c8e81e7066f92e552f
Ostap <ostapbojko04@gmail.com>
2024-07-04 22:45:16 +0300

    remove function in views.py and create new classes

--- catalog/views.py
+++ catalog/views.py
@@ -1,100 +1,66 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Task, Tag
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views import View
 from .forms import TaskForm, TagForm
+from .models import Task, Tag
 
 
-def home(request):
-    tasks = Task.objects.all().order_by("done", "-created_at")
-    return render(request, "catalog/home.html", {"tasks": tasks})
+class HomeView(ListView):
+    model = Task
+    template_name = "catalog/home.html"
+    context_object_name = "tasks"
+    ordering = ["done", "-created_at"]
 
 
-def task_form_view(request, pk=None):
-    if pk:
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "catalog/task_form.html"
+    success_url = "/"
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "catalog/task_form.html"
+    success_url = "/"
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = "catalog/tag_list.html"
+    context_object_name = "tags"
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    form_class = TagForm
+    template_name = "catalog/tag_form.html"
+    success_url = "/tags/"
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    form_class = TagForm
+    template_name = "catalog/tag_form.html"
+    success_url = "/"
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    template_name = "catalog/tag_confirm_delete.html"
+    success_url = "/"
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = "catalog/task_confirm_delete.html"
+    success_url = "/"
+
+
+class TaskToggleCompleteView(View):
+    def post(self, request, pk):
         task = get_object_or_404(Task, pk=pk)
-    else:
-        task = None
-
-    if request.method == "POST":
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-    else:
-        form = TaskForm(instance=task)
-
-    return render(request, "catalog/task_form.html", {"form": form})
-
-
-def tag_list(request):
-    tags = Tag.objects.all()
-    return render(request, "catalog/tag_list.html", {"tags": tags})
-
-
-def update_tag(request, pk):
-    tag = get_object_or_404(Tag, pk=pk)
-    if request.method == "POST":
-        form = TagForm(request.POST, instance=tag)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-    else:
-        form = TagForm(instance=tag)
-    return render(request, "catalog/tag_form.html", {"form": form})
-
-
-def delete_tag(request, pk):
-    tag = get_object_or_404(Tag, pk=pk)
-    if request.method == 'POST':
-        tag.delete()
-        return redirect('home')
-    return render(request, "catalog/tag_confirm_delete.html", {"tag": tag})
-
-
-def add_tag(request):
-    if request.method == "POST":
-        form = TagForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('tag_list')
-    else:
-        form = TagForm()
-    return render(request, "catalog/tag_form.html", {"form": form})
-
-
-def add_task(request):
-    if request.method == "POST":
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-    else:
-        form = TaskForm()
-    return render(request, "catalog/task_form.html", {"form": form})
-
-
-def update_task(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-
-    if request.method == 'POST':
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-    else:
-        form = TaskForm(instance=task)
-    return render(request, "catalog/task_form.html", {"form": form})
-
-
-def delete_task(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if request.method == "POST":
-        task.delete()
-        return redirect('home')
-    return render(request, "catalog/task_confirm_delete.html", {"task": task})
-
-
-def complete_task(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    task.done = not task.done
-    task.save()
-    return redirect("home")
+        task.done = not task.done
+        task.save()
+        return redirect("home")
